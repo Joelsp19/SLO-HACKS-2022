af94525a65872737e4e0caa0bff513ae3c6306db
Akhil Elamanchili <akhilelamanchili@Akhils-MacBook-Pro.local>
2022-04-09 11:28:41 -0700

    GUI

--- slohacksmain.py
+++ slohacksmain.py
@@ -1,5 +1,10 @@
 import tkinter as tk
 window = tk.Tk()
-greeting = tk.Label(text="Hello SLO HACK")
+greeting= tk.Label(
+    text="Hello, Tkinter",
+    foreground="white",  # Set the text color to white
+    background="black"  # Set the background color to black
+    )
 greeting.pack()
 window.mainloop()
+greeting2 = tk
