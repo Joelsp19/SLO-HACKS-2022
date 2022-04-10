6eedfed87f79f4c99f33fc8248969770c6d273c5
joelp <jputhank@calpoly.edu>
2022-04-09 18:08:08 -0700

    edits v2

--- slohacksmain.py
+++ slohacksmain.py
@@ -1,17 +1,38 @@
 from tkinter import *
 from FallingStars import *
+from GravityDash import *
 
 window = Tk(className = 'Multitask Game')
 window.geometry("1500x800")
-myCanvas = Canvas(window, width= 1500, height = 800)
-myCanvas.pack()
+c = Canvas(window, width= 1500, height = 800)
 
-
-
-fallinggame = FallingStars(myCanvas)
+fallinggame = FallingStars(c)
 fallinggame.create_stars()
 window.bind("<Left>", fallinggame.left)
 window.bind("<Right>", fallinggame.right)
 
-window.mainloop()
+gGame = GravityDash(c)
+gGame.spike_move()
+window.bind("<Up>", gGame.up)
+window.bind("<Down>", gGame.down)
 
+space = 0
+min_space_btwn_spike = 3
+
+
+def run():
+    global space
+    gGame.spike_move()
+    if (space==0):
+        gGame.create_spikes()
+        space+=1
+    elif (space !=min_space_btwn_spike):
+        space+=1
+    else:
+        space=0
+
+    window.after(200,run)
+
+c.pack()
+window.after(1000,run)
+window.mainloop()
